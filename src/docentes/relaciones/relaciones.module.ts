42727e78f1f7e51ace6c1d17b13f889c5102ae45
xkendovul <hadyihernandez@gmail.com>
2026-06-23 01:55:51 -0600

    cambio variables de entorno

---
+++ src/docentes/relaciones/relaciones.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { DOCENTE_SERVICE } from '../../config/service';
+
+@Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: DOCENTE_SERVICE,
+        transport: Transport.TCP,
+        options: {
+          host: process.env.DOCENTE_SERVICE_HOST ?? '127.0.0.1',
+          port: Number(process.env.DOCENTE_SERVICE_PORT ?? 3003),
+        },
+      },
+    ]),
+  ],
+  controllers: [],
+  providers: [],
+  exports: [],
+})
+export class DocenteRelacionesModule {}
