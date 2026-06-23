0830e541b5f6e561bad765a7e83aff6d128f59dd
xkendovul <hadyihernandez@gmail.com>
2026-06-23 02:25:35 -0600

    cambio variables de entorno

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ import { Logger } from '@nestjs/common';
 async function bootstrap() {
   const logger = new Logger('Main-Gateway');
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 
   logger.log(`Cliente Gateway corriendo en el puerto ${process.env.PORT}`);
